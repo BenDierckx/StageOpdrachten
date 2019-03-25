cf212f6cf55bf7bdbd7ae2cf3aadf9c0f295abf7
Ben Dierckx <BenDierckx1@hotmail.com>
2019-03-25 10:43:45 +0100

    Added the compare functions to MigrateJson

--- Opdracht 2 Ben/IS4U-FIM-Powershell-master/IS4U.MigrateJson/IS4U.MigrateJson.psm1
+++ Opdracht 2 Ben/IS4U-FIM-Powershell-master/IS4U.MigrateJson/IS4U.MigrateJson.psm1
@@ -97,6 +97,43 @@ Function Get-PolicyConfigToJson {
     Convert-ToJson -Objects $syncFilter -JsonName SynchronizationFilter
 }
 
+Function Compare-Policy {
+    Write-Host "Starting compare of Policy configuration..."
+    # Source of objects to be imported
+    $mgmntPlciesSrc = Get-ObjectsFromJson -xmlFilePath "ConfigPolicies.json"
+    $setsSrc = Get-ObjectsFromJson -xmlFilePath "ConfigSets.json"
+    $workflowSrc = Get-ObjectsFromJson -xmlFilePath "ConfigWorkflows.json"
+    $emailSrc = Get-ObjectsFromJson -xmlFilePath "ConfigEmailTemplates.json"
+    $filtersSrc = Get-ObjectsFromJson -xmlFilePath "ConfigFilterScopes.json"
+    $activitySrc = Get-ObjectsFromJson -xmlFilePath "ConfigActivityInfo.json"
+    $funcSrc = Get-ObjectsFromJson -xmlFilePath "ConfigPolicyFunctions.json"
+    $syncRSrc = Get-ObjectsFromJson -xmlFilePath "ConfigSyncRules.json"
+    $syncFSrc = Get-ObjectsFromJson -xmlFilePath "ConfigSyncFilters.json"
+
+    # Target Setup objects, comparing purposes
+    $mgmntPlciesDest = Search-Resources -XPath "/ManagementPolicyRule" -ExpectedObjectType ManagementPolicyRule
+    $setsDest = Search-Resources -XPath "/Set" -ExpectedObjectType Set
+    $workflowDest = Search-Resources -XPath "/WorkflowDefinition" -ExpectedObjectType WorkflowDefinition
+    $emailDest = Search-Resources -XPath "/EmailTemplate" -ExpectedObjectType EmailTemplate
+    $filtersDest = Search-Resources -XPath "/FilterScope" -ExpectedObjectType FilterScope
+    $activityDest = Search-Resources -XPath "/ActivityInformationConfiguration" -ExpectedObjectType ActivityInformationConfiguration
+    $funcDest = Search-Resources -XPath "/Function" -ExpectedObjectType Function 
+    $syncRDest = Search-Resources -XPath "/SynchronizationRule" -ExpectedObjectType SynchronizationRule
+    $syncFDest = Search-Resources -XPath "/SynchronizationFilter" -ExpectedObjectType SynchronizationFilter
+
+    # Comparing of the Source and Target Setup to create delta xml file
+    Compare-Objects -ObjsSource $mgmntPlciesSrc -ObjsDestination $mgmntPlciesDest
+    Compare-Objects -ObjsSource $setsSrc -ObjsDestination $setsDest
+    Compare-Objects -ObjsSource $workflowSrc -ObjsDestination $workflowDest
+    Compare-Objects -ObjsSource $emailSrc -ObjsDestination $emailDest 
+    Compare-Objects -ObjsSource $filtersSrc -ObjsDestination $filtersDest
+    Compare-Objects -ObjsSource $activitySrc -ObjsDestination $activityDest
+    Compare-Objects -ObjsSource $funcSrc -ObjsDestination $funcDest
+    Compare-Objects -ObjsSource $syncRSrc -ObjsDestination $syncRDest
+    Compare-Objects -ObjsSource $syncFSrc -ObjsDestination $syncFDest
+    Write-Host "Compare of Policy configuration completed."
+}
+
 Function Get-PortalConfigToJson {
     param(
         [Parameter(Mandatory=$False)]
@@ -119,6 +156,31 @@ Function Get-PortalConfigToJson {
     Convert-ToJson -Objects $naviBarConf -JsonName NavigationBarConfigurations
     Convert-ToJson -Objects $searchScopeConf -JsonName SearchScopeConfigurations
     Convert-ToJson -Objects $objectVisualConf -JsonName ObjectVisualizationConfigurations
+}
+
+Function Compare-Portal {
+    Write-Host "Starting compare of Portal configuration..."
+    # Source of objects to be imported
+    $UISrc = Get-ObjectsFromJson -xmlFilePath "ConfigPortalUI.json"
+    $navSrc = Get-ObjectsFromJson -xmlFilePath "ConfigNavBar.json"
+    $srchScopeSrc = Get-ObjectsFromJson -xmlFilePath "ConfigSearchScope.json"
+    $objVisSrc = Get-ObjectsFromJson -xmlFilePath "ConfigObjectVisual.json"
+    $homePSrc = Get-ObjectsFromJson -xmlFilePath "ConfigHomePage.json"
+
+    # Target Setup objects, comparing purposes
+    $UIDest = Search-Resources -XPath "/PortalUIConfiguration" -ExpectedObjectType PortalUIConfiguration
+    $navDest = Search-Resources -XPath "/NavigationBarConfiguration" -ExpectedObjectType NavigationBarConfiguration
+    $srchScopeDest = Search-Resources -XPath "/SearchScopeConfiguration" -ExpectedObjectType SearchScopeConfiguration
+    $objVisDest = Search-Resources -XPath "/ObjectVisualizationConfiguration" -ExpectedObjectType ObjectVisualizationConfiguration
+    $homePDest = Search-Resources -XPath "/HomepageConfiguration" -ExpectedObjectType HomepageConfiguration
+
+    # Comparing of the Source and Target Setup to create delta xml file
+    Compare-Objects -ObjsSource $UISrc -ObjsDestination $UIDest
+    Compare-Objects -ObjsSource $navSrc -ObjsDestination $navDest
+    Compare-Objects -ObjsSource $srchScopeSrc -ObjsDestination $srchScopeDest
+    Compare-Objects -ObjsSource $objVisSrc -ObjsDestination $objVisDest
+    Compare-Objects -ObjsSource $homePSrc -ObjsDestination $homePDest
+    Write-Host "Compare of Portal configuration completed."
 }
 
 Function Get-ObjectsFromConfig {
@@ -147,7 +209,7 @@ Function Convert-ToJson {
         $objMembers = $obj.psobject.members | Where-Object membertype -Like 'noteproperty'
         $obj = $objMembers
     }
-    ConvertTo-Json -InputObject $Objects -Depth 4 -Compress | Out-File "./Json$JsonName.json"      # "./IS4U.MigrateTest"
+    ConvertTo-Json -InputObject $Objects -Depth 4 -Compress | Out-File "./$JsonName.json"
 }
 
 Function Get-ObjectsFromJson {
@@ -159,6 +221,134 @@ Function Get-ObjectsFromJson {
 
     $objs = Get-Content $JsonFilePath | ConvertFrom-Json
     return $objs
+}
+
+Function Compare-Objects {
+    param (
+        [Parameter(Mandatory=$True)]
+        [array]
+        $ObjsSource,
+
+        [Parameter(Mandatory=$True)]
+        [array]
+        $ObjsDestination
+    )
+
+    $difference = [System.Collections.ArrayList] @()
+    foreach ($obj in $ObjsSource){
+        $obj2 = $ObjsDestination | Where-Object {$_.Name -eq $obj.Name}
+        if (!$obj2) {
+            $difference.Add($obj)
+        }
+        $compResult = Compare-Object -ReferenceObject $obj.psobject.members -DifferenceObject $obj2.psobject.members -PassThru
+        $compObj = $compResult | Where-Object {$_.SideIndicator -eq '<='} # Difference from original!
+        $compObj = $compObj | Where-Object membertype -like 'noteproperty'
+        $newObj = [PsCustomObject] @{}
+        foreach($prop in $compObj){
+            $newobj | Add-Member -NotePropertyName $prop.Name -NotePropertyValue $prop.Value
+           }
+        Write-host "Different object:"
+        Write-host $newObj   
+        $difference.Add($newObj)
+    }
+    Write-ToXmlFile -DifferenceObject $difference
+}
+
+Function Write-ToXmlFile {
+    param (
+        [Parameter(Mandatory=$True)]
+        [System.Collections.ArrayList]
+        $DifferenceObjects
+    )
+    # Inititalization xml file
+    $FileName = "configurationDelta.xml"
+    # Create empty starting lithnet configuration xml file
+    if (!(Test-Path -Path $FileName)) {
+        [xml]$Doc = New-Object System.Xml.XmlDocument
+        $initalElement = $Doc.CreateElement("Lithnet.ResourceManagement.ConfigSync")
+        $operationsElement = $Doc.CreateElement("Operations")
+        $dec = $Doc.CreateXmlDeclaration("1.0","UTF-8",$null)
+        $Doc.AppendChild($dec)
+        $startNode = $Doc.AppendChild($initalElement)
+        $startNode.AppendChild($operationsElement)
+        $Doc.Save("./IS4U.Migrate/configurationDelta.xml")
+    }
+    if (!(Test-Path -Path $FileName)) {
+        Write-Host "File not found"
+        break
+    }
+    $XmlDoc = [System.Xml.XmlDocument] (Get-Content $FileName)
+    $node = $XmlDoc.SelectSingleNode('//Operations')
+
+    # Place objects in XML file
+    # Iterate over the array of arrays of PsCustomObjects
+    foreach ($obj in $DifferenceObjects) {
+        # Operation description
+        $xmlElement = $XmlDoc.CreateElement("ResourceOperation")
+        $XmlOperation = $node.AppendChild($xmlElement)
+        $XmlOperation.SetAttribute("operation", "Add Update")
+        $XmlOperation.SetAttribute("resourceType", $Obj.ObjectType)
+        # Anchor description
+        $xmlElement = $XmlDoc.CreateElement("AnchorAttributes")
+        $XmlAnchors = $XmlOperation.AppendChild($xmlElement)
+            # Different anchors for Bindings (referentials)
+        if ($obj.ObjectType -eq "BindingDescription") {
+            $xmlElement1 = $XmlDoc.CreateElement("AnchorAttribute")
+            $xmlElement1.Set_InnerText("BoundAttributeType")
+            $xmlElement2 = $XmlDoc.CreateElement("AnchorAttribute")
+            $xmlElement2.Set_InnerText("BoundObjectType")
+            $XmlAnchors.AppendChild($xmlElement1)
+            $XmlAnchors.AppendChild($xmlElement2)
+        } else {
+        $xmlElement = $XmlDoc.CreateElement("AnchorAttribute")
+        $xmlElement.Set_InnerText("Name")
+        $XmlAnchors.AppendChild($xmlElement)
+        }
+        # Attributes of the object
+        $xmlEle = $XmlDoc.CreateElement("AttributeOperations")
+        $XmlAttributes = $XmlOperation.AppendChild($xmlEle)
+        # Get the PsCustomObject members from the MIM service without the hidden/extra members
+        $objMembers = $obj.psobject.Members | Where-Object membertype -like 'noteproperty'
+        # iterate over the PsCustomObject members and append them to the AttributeOperations element
+        foreach ($member in $objMembers) {
+            if ($member.Value.GetType().BaseType.Name -eq "Array") {  ## aangepast, beziet altijd of het array is of niet
+                foreach ($m in $member.Value) {
+                    $xmlVarElement = $XmlDoc.CreateElement("AttributeOperation")
+                    $xmlVarElement.Set_InnerText($m)
+                    $xmlVariable = $XmlAttributes.AppendChild($xmlVarElement)
+                    $xmlVariable.SetAttribute("operation", "add") # add because we don't want to replace the items 
+                    $xmlVariable.SetAttribute("name", $member.Name)
+                }
+            }
+            # Attributes that are read only do not get implemented in the xml file
+            $illegalMembers = @("ObjectType", "CreatedTime", "Creator", "DeletedTime", "DetectedRulesList",
+             "ExpectedRulesList", "ResourceTime")
+            # Skip read only attributes and ObjectType (already used in ResourceOperation)
+            if ($illegalMembers -contains $member.Name) { continue }
+            # referencing purposes, no need in the attributes itself (Lithnet does this)
+            if ($member.Name -eq "ObjectID") {
+                # set the objectID of the object as the id of the xml node
+                $XmlOperation.SetAttribute("id", $member.Value)
+                continue # Import-RmConfig creates an objectID in the new setup
+            }
+            $xmlVarElement = $XmlDoc.CreateElement("AttributeOperation")
+            $xmlVarElement.Set_InnerText($member.Value)
+            $xmlVariable = $XmlAttributes.AppendChild($xmlVarElement)
+            $xmlVariable.SetAttribute("operation", "replace") #Add of replace?
+            $xmlVariable.SetAttribute("name", $member.Name)
+            if ($member.Name -eq "BoundAttributeType" -or $member.Name -eq "BoundObjectType") {
+                $xmlVariable.SetAttribute("type", "xmlref")
+            }
+        }
+    }
+    # Save the xml 
+    $XmlDoc.Save("./IS4U.Migrate/ConfigurationDelta.xml")
+    # Confirmation
+    Write-Host "Written differences in objects to the delta xml file(ConfiurationDelta.xml)"
+    # Return the new xml 
+    #[xml]$result = $XmlDoc | Select-Xml -XPath "//ResourceOperation[@resourceType='$ObjectType']"
+    #[xml]$result = [System.Xml.XmlDocument] (Get-Content ".\$ObjectType.xml")
+    #return $result
 }
 
 Function Import-Delta {
